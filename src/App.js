577dde68f22df66b98aa8024443f6c13f67a9e5a
sandeep <sandeep.pr3@gmail.com>
2022-11-25 20:05:24 +0530

    netflixx

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import navBar from './components/navBar/navBar'
+
+function App() {
+  return (
+    <div className="App">
+      <navBar/>
+      
+    </div>
+  )
+}
+
+export default App
